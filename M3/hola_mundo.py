a93b9b96eacef0fe0881555392885c0547f7608c
Leik Caro <leikcaro@gmail.com>
2025-06-19 20:36:19 -0400

    hola mundo!

--- M3/hola_mundo.py
+++ M3/hola_mundo.py
@@ -1,1 +1,19 @@
+print("Hola, mundo!")
 
+x=4
+y=5
+print("la suma de x e y es", x+y)
+#la multiplicacion de x e y es
+print("la multiplicacion de x e y es", x*y)
+#la resta de x e y es
+print("la resta de x e y es", x-y)
+#la division de x e y es
+print("la division de x e y es", x/y)
+
+#la potencia de x e y es
+print("la potencia de x e y es", x**y)
+
+#la division entera de x e y es
+print("la division entera de x e y es", x//y)   
+#el modulo de x e y es
+print("el modulo de x e y es", x%y)
