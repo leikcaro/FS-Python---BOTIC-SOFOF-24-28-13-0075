6f7d20371a8c141c900762c394b23b1a64272551
Leik Caro <leikcaro@gmail.com>
2025-06-23 20:15:55 -0400

    interpolacion.py: Implementación de interpolación

---
+++ M3/interpolacion.py
@@ -0,0 +1,19 @@
+# Ejemplo de uso de print()
+
+nombre = "Ana"
+edad = 25
+print("Nombre:", nombre)
+print("Edad:", edad)
+print(f"Hola {nombre}, tienes {edad} años")  
+
+print("Hola", nombre, "tienes", edad, "años") # RECUERDEN QUE print() PUEDE RECIBIR VARIOS ARGUMENTOS
+''' Ejemplo de uso de type() y upper()
+asdasdasdads
+asdasdasda
+asdadasdad'''
+texto = "python" 
+
+# Ejemplo de uso de type() y upper()
+print("El tipo de dato de texto es:", type(texto))
+# Convertir texto a mayúsculas
+print(texto.upper())
