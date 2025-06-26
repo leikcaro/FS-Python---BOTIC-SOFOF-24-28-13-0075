6e09f0fb91a6ff4fc3cf103dcdcdcbd4134713fa
Leik Caro <leikcaro@gmail.com>
2025-06-25 20:11:44 -0400

    paquetes y modulos

---
+++ M3/calculadora/calculadora/operaciones_avanzadas.py
@@ -0,0 +1,9 @@
+def potencia(a, b=5):
+    print(f"------Calculando potencia de {a} a {b}-----")
+    return a ** b
+
+def raiz_cuadrada(numero):
+    return numero ** 0.5
+
+def saludo():
+    return "¡Hola! Soy la calculadora avanzada."
---
+++ M3/calculadora/main.py
@@ -0,0 +1,7 @@
+from calculadora import operaciones_basicas, operaciones_avanzadas
+
+print(operaciones_avanzadas.saludo())
+print("Suma:", operaciones_basicas.suma(4, 2))
+print("Resta:", operaciones_basicas.resta(4, 2))
+print("Potencia:", operaciones_avanzadas.potencia(2,3))
+print("Raíz cuadrada:", operaciones_avanzadas.raiz_cuadrada(9))
